2bdf7d54b2b323ecee6723e3ffab2b5533161066
Mohammad Jafar <itzafar65@gmail.com>
2024-02-25 10:33:37 +0530

    server

--- frontend/backend/server.js
+++ frontend/backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 require('dotenv').config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 443;
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
