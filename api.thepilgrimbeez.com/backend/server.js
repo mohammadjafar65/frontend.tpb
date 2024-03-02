7dee97bf80d7c6c49c4e0ae0ac656a863ada6c76
Mohammad Jafar <itzafar65@gmail.com>
2024-03-02 16:26:37 +0530

    sdsd dfsdf sdf

--- api.thepilgrimbeez.com/backend/server.js
+++ api.thepilgrimbeez.com/backend/server.js
@@ -1,25 +1,23 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const multer = require("multer");
 const fs = require("fs");
 const mysql = require("mysql");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
-const jwt = require('jsonwebtoken');
 
 const app = express();
-app.use(cors());
-
+app.use(cors()); // CORS middleware applied here
 // Set up CORS headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Change * to your specific origin if needed
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Change * to your specific origin if needed
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 app.use(express.json());
 
-require("dotenv").config();
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
@@ -30,24 +28,21 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
+db.connect(err => {
   if (err) {
-    console.error("Database connection failed: " + err.stack);
+    console.error('Database connection failed: ' + err.stack);
     return;
   }
-  console.log("Connected to database.");
+  console.log('Connected to database.');
 });
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
 });
 
 const upload = multer({ storage: storage });
@@ -64,11 +59,12 @@ app.use("/static", express.static(path.join(__dirname, "public")));
 // Middleware to handle errors globally
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: err.message }); // Send the error message as JSON response
+  res.status(500).send('Something went wrong!');
 });
 
-require("./travelPackages")(app, db, upload, uuidv4);
-require("./loginApi")(app, db);
+// Routes and middleware for handling travel packages
+require('./travelPackages')(app, db, upload, uuidv4);
+
 
 // Other routes and logic...
 app.listen(port, () => {
