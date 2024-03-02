ef82b70f2746e5f621a94e360da9a90db13d6d85
Mohammad Jafar <itzafar65@gmail.com>
2024-03-02 14:18:32 +0530

    jh gjkhg jg jhv jhvjh

--- tpb-admin/src/App.js
+++ tpb-admin/src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./dashboard.js";
-import './App.css';
+import "./App.css";
+import LoginScreen from "./LoginScreen.js";
 
 function App() {
   return (
-    <div className="App">        
-        <BrowserRouter>
-          <Routes>
-              {/* <Route path="/" component={MaintenancePage} /> */}
-              <Route path="/" element={<Dashboard/>}></Route>
-          </Routes>
-        </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" component={MaintenancePage} /> */}
+          <Route path="/" element={<LoginScreen />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
