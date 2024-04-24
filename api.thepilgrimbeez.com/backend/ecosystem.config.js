043a6bce087d906f1be82e80ff90b0c691f16050
Mohammad Jafar <itzafar65@gmail.com>
2024-04-24 17:56:18 +0530

    ccd

--- api.thepilgrimbeez.com/backend/ecosystem.config.js
+++ api.thepilgrimbeez.com/backend/ecosystem.config.js
@@ -6,8 +6,23 @@ module.exports = {
       instances: "max",
       autorestart: true,
       watch: true,
-      max_memory_restart: "1G", // Corrected memory limit to 1 gigabyte
-      kill_timeout: 10000, // 10 seconds
+      max_memory_restart: "1G",
+      kill_timeout: 10000,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "mytest",
+      script: "./test.js",
+      instances: 1, // You can specify the number of instances for test.js
+      autorestart: true,
+      watch: false, // Set to true if you want PM2 to watch for changes in test.js
+      max_memory_restart: "500M", // Example memory limit for test.js
+      kill_timeout: 5000, // Example kill timeout for test.js
       env: {
         NODE_ENV: "development",
       },
