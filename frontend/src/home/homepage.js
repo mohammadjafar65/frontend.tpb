9716ddc3a1efddaf8b738fc53e4360b08a90da8c
Mohammad Jafar <itzafar65@gmail.com>
2024-05-01 19:41:58 +0530

    update 2.66

--- frontend/src/home/homepage.js
+++ frontend/src/home/homepage.js
@@ -103,75 +103,85 @@ function HomePage() {
                 </div>
             </section>
             {/* Packages by Category */}
-            {Object.keys(packagesByCategory).map((category, index) => (
-                <section id="our-packages" key={category} className={index % 2 === 0 ? "alternate-class" : "gray_bg"}>
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-lg-12 col-md-12 col-12">
-                                <div className="title_head">
-                                    <h2>{category || "Default Category"}</h2>
-                                    <Link to={`/package/${encodeURIComponent(category)}`} className="btn">
-                                        View All &nbsp;<iconify-icon icon="cil:arrow-right"></iconify-icon>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-lg-12 col-md-12 col-12">
-                                <OwlCarousel
-                                    className="owl-carousel owl-theme projects"
-                                    margin={30}
-                                    nav
-                                    responsive={{
-                                        0: {
-                                            items: 1,
-                                        },
-                                        600: {
-                                            items: 3,
-                                        },
-                                        1000: {
-                                            items: 5,
-                                        },
-                                    }}
-                                >
-                                    {packagesByCategory[category] && packagesByCategory[category].length > 0 ? (
-                                        packagesByCategory[category].map((pkg) => (
-                                            <div className="item" key={pkg.id}>
-                                                <Link to={`/package/id/${pkg.id}`}>
-                                                    <div className="card">
-                                                        <span className="over_hover">
-                                                            <img src={`${process.env.REACT_APP_API_URL}/uploads/${pkg.imageUrl}`} alt={pkg.packageName || "Package Image"} className="card-img" />
-                                                        </span>
-                                                        <div className="card_content">
-                                                            <h2>{pkg.packageName || "No Name"}</h2>
-                                                            <p>{pkg.packagePrice ? `₹${pkg.packagePrice}` : "Not available"}</p>
-                                                            <div className="ft">
-                                                                <span>
-                                                                    <label>Duration</label>
-                                                                    <h3>
-                                                                        <iconify-icon icon="carbon:time"></iconify-icon> {pkg.packageDurationDate || "Not available"}
-                                                                    </h3>
-                                                                </span>
-                                                                <span>
-                                                                    <label>Start Date</label>
-                                                                    <h3>
-                                                                        <iconify-icon icon="uiw:date"></iconify-icon> {pkg.packageDate ? formatDate(pkg.packageDate) : "Not available"}
-                                                                    </h3>
-                                                                </span>
-                                                            </div>
-                                                            <div className="btn_yellow">View Package Details</div>
-                                                        </div>
-                                                    </div>
-                                                </Link>
-                                            </div>
-                                        ))
-                                    ) : (
-                                        <p>No packages available.</p>
-                                    )}
-                                </OwlCarousel>
-                            </div>
+            {categories.map((category, index) => (
+                <section
+                key={category} id="our-packages" className={index % 2 === 0 ? "alternate-class" : "gray_bg"}
+                >
+                <div className="container-fluid">
+                    <div className="row">
+                    <div className="col-lg-12 col-md-12 col-12">
+                        <div className="title_head">
+                        <h2>{category || "Default Category"}</h2>
+                        <Link
+                            to={`/package/${encodeURIComponent(category)}`}
+                            className="btn"
+                        >
+                            View All &nbsp;
+                            <iconify-icon icon="cil:arrow-right"></iconify-icon>
+                        </Link>
                         </div>
                     </div>
+                    </div>
+                    <div className="row">
+                    <div className="col-lg-12 col-md-12 col-12">
+                        <OwlCarousel
+                        className="owl-carousel owl-theme projects"
+                        margin={30}
+                        nav
+                        responsive={{
+                            0: {
+                            items: 1,
+                            },
+                            600: {
+                            items: 3,
+                            },
+                            1000: {
+                            items: 5,
+                            },
+                        }}
+                        >
+                        {packagesByCategory[category] && packagesByCategory[category].length > 0 ? (
+                            packagesByCategory[category].map((pkg) => (
+                            <div className="item" key={pkg.id}>
+                                <Link to={`/package/id/${pkg.id}`}>
+                                <div className="card">
+                                    <span className="over_hover">
+                                    <img
+                                        src={`${process.env.REACT_APP_API_URL}/uploads/${pkg.imageUrl}`}
+                                        alt={pkg.packageName || "Package Image"}
+                                        className="card-img"
+                                    />
+                                    </span>
+                                    <div className="card_content">
+                                    <h2>{pkg.packageName || "No Name"}</h2>
+                                    <p>{pkg.packagePrice ? `₹${pkg.packagePrice}` : "Not available"}</p>
+                                    <div className="ft">
+                                        <span>
+                                        <label>Duration</label>
+                                        <h3>
+                                            <iconify-icon icon="carbon:time"></iconify-icon> {pkg.packageDurationDate || "Not available"}
+                                        </h3>
+                                        </span>
+                                        <span>
+                                        <label>Start Date</label>
+                                        <h3>
+                                            <iconify-icon icon="uiw:date"></iconify-icon> {pkg.packageDate ? formatDate(pkg.packageDate) : "Not available"}
+                                        </h3>
+                                        </span>
+                                    </div>
+                                    <div className="btn_yellow">View Package Details</div>
+                                    </div>
+                                </div>
+                                </Link>
+                            </div>
+                            ))
+                        ) : (
+                            <p>No packages available.</p>
+                        )}
+                        </OwlCarousel>
+                    </div>
+                    </div>
+                </div>
                 </section>
             ))}
             <section id="about_us">
