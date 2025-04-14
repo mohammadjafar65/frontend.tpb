0e135e8eb5a825dce849afed8b428f1a884b79ed
Mohammad Jafar <itzafar65@gmail.com>
2025-04-14 13:35:58 +0530

    update

--- frontend/src/ComingSoon.js
+++ frontend/src/ComingSoon.js
@@ -6,12 +6,7 @@ const ComingSoon = () => {
     <div className="container">
       <h1>Coming soon.</h1>
       <p className="description">
-        With our solution you don’t need to care about anything.
-        <br />
-        The whole idea is simple as possible and will be easy to use for
-        everyone
-        <br />
-        on this planet. Check the video below to see more!
+        We are working hard on it!!!
       </p>
       <div className="progress-bar">
         <div className="progress-fill" style={{ width: "25%" }}>
@@ -19,11 +14,11 @@ const ComingSoon = () => {
         </div>
       </div>
 
-      <div className="socials">
+      {/* <div className="socials">
         <button className="social-btn fb">👍 Like</button>
         <button className="social-btn tw">🐦 Tweet</button>
         <button className="social-btn gp">🔗 +1</button>
-      </div>
+      </div> */}
     </div>
   );
 };
